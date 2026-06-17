0e013969d1e492927ac6153063d6aea54789a953
saleh282 <saleh.salehh567@gmail.com>
2026-06-17 16:19:43 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,13 +9,13 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../frontend')));
+app.use(express.static(path.join(__dirname, 'frontend')));
 app.use('/api/auth',      require('./routes/auth'));
 app.use('/api/days',      require('./routes/days'));
 app.use('/api/exercises', require('./routes/exercises'));
 
 app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '../frontend/index.html'))
+  res.sendFile(path.join(__dirname, 'frontend', 'index.html'))
 );
 
 mongoose.connect(process.env.MONGODB_URI)
